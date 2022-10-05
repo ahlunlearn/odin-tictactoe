5a5c354d352205861a52ee01a45d6db8caf0a08f
luan <lunchiuah@gmail.com>
2022-10-05 16:48:54 +0800

    put mark function on

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gameBoard = (()=>{
-    // const board = Array(9).fill('')
-    const board = ['X','X','X','X','X','X','X','X','X']
+    const board = Array(9).fill('')
 
     const setMark = (id, mark) => {
         board[id] = mark
@@ -19,19 +18,22 @@ const gameBoard = (()=>{
 const displayController = (()=>{
     const updateaGrid = (id, mark) => {
         gameBoard.setMark(id, mark)
+        document.querySelector('.gameBoard').innerText = ''
+        displayBoard()
     }
 
     const displayBoard = () =>{
-        console.log('get')
         const boardContent = gameBoard.getBoardStatus()
         const gameBoardDom = document.querySelector('.gameBoard')
         for(let i=0;i<boardContent.length;i++){
             let item = document.createElement('div')
             item.classList.add('item')
+            item.setAttribute('id',i.toString())
             item.innerText = boardContent[i]
             gameBoardDom.appendChild(item)
         }
     }
+
 
     return {
         updateaGrid,
@@ -41,4 +43,16 @@ const displayController = (()=>{
 
 displayController.displayBoard()
 
-// const Player = ()
+const Player = (mark) => {
+    const click = (id) =>{
+        displayController.updateaGrid(id,mark)
+        console.log()
+    }
+    return {click}
+}
+const P1 = Player('O')
+document.querySelector('.gameBoard').addEventListener('click',(e)=>{
+    if(e.target.classList.contains('item') && e.target.innerText === ''){
+    P1.click(Number(e.target.getAttribute('id')))
+    }
+})
